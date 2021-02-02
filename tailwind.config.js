7b3965ff53a26a273893bd28c5322ba64dafdc80
Robert Reinert <robert.reinert@gmail.com>
2021-02-02 15:14:22 -0800

    webpack setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./views/*.pug", "./public/javascripts/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
